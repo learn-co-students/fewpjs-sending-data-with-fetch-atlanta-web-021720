e0e17e92dfe143e519be07ce5c2bd7d29da261bc
arieldavis22 <aadavisbusiness@gmail.com>
2020-04-01 21:42:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(username, userEmail) {
+    let formData = {
+        name: username,
+        email: userEmail
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let newObj = document.createElement("h1");
+        newObj.innerHTML = object.id
+        document.querySelector("body").appendChild(newObj);
+    })
+    .catch(function(error) {
+        alert("Everthing is on fire.");
+        let newErr = document.createElement('h1');
+        newErr.innerHTML = error.message;
+        document.querySelector("body").appendChild(newErr);
+    });
+
+}
